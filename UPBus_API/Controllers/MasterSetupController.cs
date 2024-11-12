dff4a3673f26ab8c5bc9d555020876edf057c081
Aung Myo Myint <aungmyomyint@myanmareasenet.com>
2024-11-12 10:56:04 +0630

    add master

--- UPBus_API/Controllers/MasterSetupController.cs
+++ UPBus_API/Controllers/MasterSetupController.cs
@@ -52,5 +52,136 @@ namespace UPBus_API.Controllers
         }
 
         #endregion
+
+        #region Gate 9-Nov-2024
+
+        [HttpGet]
+        public async Task<IActionResult> GetGateList()
+        {
+            DataTable dt = await _service.GetGateList();
+            return Ok(dt);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGate(GateDto info)
+        {
+            ResponseMessage msg = await _service.CreateGate(info);
+            return Ok(msg);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateGate(GateDto info)
+        {
+            ResponseMessage msg = await _service.UpdateGate(info);
+            return Ok(msg);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGate(string id)
+        {
+            ResponseMessage msg = await _service.DeleteGate(id);
+            return Ok(msg);
+        }
+
+        #endregion
+
+        #region Expense Type 11-Nov-2024
+
+        [HttpGet]
+        public async Task<IActionResult> GetExpenseTypeList()
+        {
+            DataTable dt = await _service.GetExpenseTypeList();
+            return Ok(dt);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveExpenseType(ExpenseTypeDto info)
+        {
+            ResponseMessage msg = await _service.SaveExpenseType(info);
+            return Ok(msg);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateExpenseType(ExpenseTypeDto info)
+        {
+            ResponseMessage msg = await _service.UpdateExpenseType(info);
+            return Ok(msg);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpenseType(string id)
+        {
+            var msg = await _service.DeleteExpenseType(id);
+            return Ok(msg);
+        }
+
+
+        #endregion
+
+        #region Income Type 11-Nov-2024
+
+        [HttpGet]
+        public async Task<IActionResult> GetIncomeTypeList()
+        {
+            DataTable dt = await _service.GetIncomeTypeList();
+            return Ok(dt);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveIncomeType(IncomeTypeDto info)
+        {
+            ResponseMessage msg = await _service.SaveIncomeType(info);
+            return Ok(msg);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateIncomeType(IncomeTypeDto info)
+        {
+            ResponseMessage msg = await _service.UpdateIncomeType(info);
+            return Ok(msg);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIncomeType(string id)
+        {
+            var msg = await _service.DeleteIncomeType(id);
+            return Ok(msg);
+        }
+
+
+        #endregion
+
+        #region Trip Type 11-Nov-2024
+
+        [HttpGet]
+        public async Task<IActionResult> GetTripTypeList()
+        {
+            DataTable dt = await _service.GetTripTypeList();
+            return Ok(dt);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveTripType(TripTypeDto info)
+        {
+            ResponseMessage msg = await _service.SaveTripType(info);
+            return Ok(msg);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTripType(TripTypeDto info)
+        {
+            ResponseMessage msg = await _service.UpdateTripType(info);
+            return Ok(msg);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTripType(string id)
+        {
+            var msg = await _service.DeleteTripType(id);
+            return Ok(msg);
+        }
+
+
+        #endregion
     }
 }
--- UPBus_API/Services/MasterSetupService.cs
+++ UPBus_API/Services/MasterSetupService.cs
@@ -187,6 +187,424 @@ namespace UPBus_API.Services
 
         #endregion
 
+        #region Gate 9_Nov_2024
+
+        public async Task<DataTable> GetGateList()
+        {
+            string sql = "SELECT * FROM Gate";
+            DataTable dt = await GetDataTableAsync(sql);
+            return dt;
+        }
+
+        public async Task<ResponseMessage> CreateGate(GateDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                Gate data = await _context.Gate.FromSqlRaw("SELECT * FROM Gate WHERE GateCode=@gateCode", new SqlParameter("@gateCode", info.GateCode)).SingleOrDefaultAsync();
+                if (data != null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Gate No duplicate!";
+                }
+                else
+                {
+
+                    Gate gate = _mapper.Map<Gate>(info);
+                    gate.CreatedDate = GetLocalStdDT();
+                    gate.CreatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.Gate.Add(gate);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully created!";
+
+                }
+
+
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+
+            return msg;
+        }
+
+        public async Task<ResponseMessage> UpdateGate(GateDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                Gate gate = await _context.Gate.FromSqlRaw("SELECT * FROM Gate WHERE GateCode=@gateCode", new SqlParameter("@gateCode", info.GateCode)).SingleOrDefaultAsync();
+                if (gate == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data not found!";
+                }
+                else
+                {
+
+                    gate.GateName = info.GateName;
+                    gate.Active = info.Active;
+                    gate.UpdatedDate = GetLocalStdDT();
+                    gate.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully updated!";
+
+
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+
+            return msg;
+        }
+
+        public async Task<ResponseMessage> DeleteGate(string id)
+        {
+            var msg = new ResponseMessage { Status = false };
+            try
+            {
+                var gate = await _context.Gate.FirstOrDefaultAsync(t => t.GateCode == id);
+
+                if (gate == null)
+                {
+                    msg.MessageContent = "Data not found.";
+                    return msg;
+                }
+
+                _context.Gate.Remove(gate);
+                await _context.SaveChangesAsync();
+
+                msg.Status = true;
+                msg.MessageContent = "Removed successfully.";
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent = $"An error occurred: {e.Message}";
+            }
+
+            return msg;
+        }
+
+        #endregion
+
+        #region Expense Type 11-Nov-2024
+
+        public async Task<DataTable> GetExpenseTypeList()
+        {
+
+            string sql = "SELECT * FROM ExpenseType";
+            DataTable dt = await GetDataTableAsync(sql);
+            return dt;
+        }
+
+        public async Task<ResponseMessage> SaveExpenseType(ExpenseTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                ExpenseType expenseType = await _context.ExpenseType.FromSqlRaw("SELECT * FROM ExpenseType WHERE ExpCode=@expCode", new SqlParameter("@expCode", info.ExpCode)).SingleOrDefaultAsync();
+                if (expenseType != null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Expense No duplicate!";
+                }
+                else
+                {
+
+                    ExpenseType data = _mapper.Map<ExpenseType>(info);
+                    data.CreatedDate = GetLocalStdDT();
+                    data.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.ExpenseType.Add(data);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully created!";
+
+                }
+
+
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+
+            return msg;
+        }
+
+        public async Task<ResponseMessage> UpdateExpenseType(ExpenseTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                ExpenseType expenseType = await _context.ExpenseType.FromSqlRaw("SELECT * FROM ExpenseType WHERE REPLACE(ExpName,' ','')=REPLACE(@name,' ','') ", new SqlParameter("@name", info.ExpName)).SingleOrDefaultAsync();
+                if (expenseType == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data Not Found";
+                }
+                else
+                {
+                    expenseType.ExpName = info.ExpName;
+                    expenseType.ExpType = info.ExpType;
+                    expenseType.Active = info.Active;
+                    expenseType.UpdatedDate = GetLocalStdDT();
+                    expenseType.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.ExpenseType.Update(expenseType);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully updated";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+            return msg;
+        }
+
+        public async Task<ResponseMessage> DeleteExpenseType(string id)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                var expenseType = await _context.ExpenseType.FirstOrDefaultAsync(t => t.ExpCode == id);
+                if (expenseType == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data Not Found";
+                }
+                else
+                {
+                    _context.ExpenseType.Remove(expenseType);
+
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully Deleted!";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+
+            return msg;
+        }
+
+        #endregion
+
+        #region Income Type 11-Nov-2024
+
+        public async Task<DataTable> GetIncomeTypeList()
+        {
+            string sql = "SELECT * FROM IncomeType";
+            DataTable dt = await GetDataTableAsync(sql);
+            return dt;
+        }
+
+        public async Task<ResponseMessage> SaveIncomeType(IncomeTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                IncomeType incomeType = await _context.IncomeType.FromSqlRaw("SELECT Top 1 * FROM IncomeType WHERE REPLACE(IncName,'','')=REPLACE(@name,'','')", new SqlParameter("@name", info.IncName)).SingleOrDefaultAsync();
+                if (incomeType != null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Name duplicate!";
+                }
+                else
+                {
+                    IncomeType type = _mapper.Map<IncomeType>(info);
+                    type.CreatedDate = GetLocalStdDT();
+                    type.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.IncomeType.Add(type);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully added";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+            return msg;
+        }
+
+        public async Task<ResponseMessage> UpdateIncomeType(IncomeTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                IncomeType expenseType = await _context.IncomeType.FromSqlRaw("SELECT * FROM IncomeType WHERE REPLACE(IncName,' ','')=REPLACE(@name,' ','') ", new SqlParameter("@name", info.IncName)).SingleOrDefaultAsync();
+                if (expenseType == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data Not Found";
+                }
+                else
+                {
+                    expenseType.IncName = info.IncName;
+                    expenseType.IncType = info.IncType;
+                    expenseType.Active = info.Active;
+                    expenseType.UpdatedDate = GetLocalStdDT();
+                    expenseType.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.IncomeType.Update(expenseType);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully updated";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+            return msg;
+        }
+
+        public async Task<ResponseMessage> DeleteIncomeType(string id)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                var incomeType = await _context.IncomeType.FirstOrDefaultAsync(t => t.IncCode == id);
+
+                if (incomeType == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data Not Found";
+                }
+                else
+                {
+                    _context.IncomeType.Remove(incomeType);
+
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully Deleted!";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+
+            return msg;
+        }
+
+        #endregion
+
+        #region Trip Type 11-Nov-2024
+
+        public async Task<DataTable> GetTripTypeList()
+        {
+            string sql = "SELECT * FROM TripType";
+            DataTable dt = await GetDataTableAsync(sql);
+            return dt;
+        }
+
+        public async Task<ResponseMessage> SaveTripType(TripTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                TripType tripType = await _context.TripType.FromSqlRaw("SELECT Top 1 * FROM TripType WHERE REPLACE(TripName,'','')=REPLACE(@name,'','')", new SqlParameter("@name", info.TripName)).SingleOrDefaultAsync();
+                if (tripType != null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Name duplicate!";
+                }
+                else
+                {
+                    TripType type = _mapper.Map<TripType>(info);
+                    type.CreatedDate = GetLocalStdDT();
+                    type.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.TripType.Add(type);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully added";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+            return msg;
+        }
+
+        public async Task<ResponseMessage> UpdateTripType(TripTypeDto info)
+        {
+            ResponseMessage msg = new ResponseMessage { Status = false };
+            try
+            {
+                TripType tripType = await _context.TripType.FromSqlRaw("SELECT * FROM TripType WHERE REPLACE(TripName,' ','')=REPLACE(@name,' ','') ", new SqlParameter("@name", info.TripName)).SingleOrDefaultAsync();
+                if (tripType == null)
+                {
+                    msg.Status = false;
+                    msg.MessageContent = "Data Not Found";
+                }
+                else
+                {
+                    tripType.TripName = info.TripName;
+                    tripType.TrpType = info.TrpType;
+                    tripType.Active = info.Active;
+                    tripType.UpdatedDate = GetLocalStdDT();
+                    tripType.UpdatedUser = _httpContextAccessor.HttpContext?.User.Identity.Name ?? "UnknownUser";
+                    _context.TripType.Update(tripType);
+                    await _context.SaveChangesAsync();
+                    msg.Status = true;
+                    msg.MessageContent = "Successfully updated";
+                }
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent += e.Message;
+                return msg;
+            }
+            return msg;
+        }
+
+        public async Task<ResponseMessage> DeleteTripType(string id)
+        {
+            var msg = new ResponseMessage { Status = false };
+            try
+            {
+                var trip = await _context.TripType.FirstOrDefaultAsync(t => t.TripCode == id);
+
+                if (trip == null)
+                {
+                    msg.MessageContent = "Data not found.";
+                    return msg;
+                }
+
+                _context.TripType.Remove(trip);
+                await _context.SaveChangesAsync();
+
+                msg.Status = true;
+                msg.MessageContent = "Removed successfully.";
+            }
+            catch (DbUpdateException e)
+            {
+                msg.MessageContent = $"An error occurred: {e.Message}";
+            }
+
+            return msg;
+        }
+
+        #endregion
+
 
     }
 }
